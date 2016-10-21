5ba28741cfd58db7ee9470b573fa0e25ffc0af02
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2016-10-21 15:39:32 -0700

    Propagate references and improve misc functionality

--- ssa.rkt
+++ ssa.rkt
@@ -30,14 +30,24 @@
 ; SSA optimization passes
 
 (define (fold-propagate-ssa ssa)
-  (let ((next-ssa (map fold-line (propagate-constants ssa))))
+  (let ((next-ssa (map fold-line (propogate-references (propagate-constants ssa)))))
     (if (equal? ssa next-ssa) ssa (fold-propagate-ssa next-ssa))))
 
 (define (propagate-constants ssa)
-  (let ((constants (filter (lambda (line) (equal? (third line)"#")) ssa)))
+  (let ((constants (filter (lambda (line) (equal? (third line) "#")) ssa)))
     (substitute-ssa ssa (map (lambda (line)
-                               (list (list "%" (second line)) (list "#" (fourth line))))
+                               (list (list "%" (second line)) (list (third line) (fourth line))))
                              constants))))
+
+(define (propogate-references ssa)
+  (map (lambda (line)
+         (if (equal? (third line) "%")
+           (append (take line 2) (lookup ssa (fourth line)))
+           line))
+       ssa))
+
+(define (lookup ssa number)
+  (drop (first (filter (lambda (k) (= (second k) number)) ssa)) 2))
 
 (define (fold-line line)
   (cond ((and (equal? (third line) "=") (equal? (first (fourth line)) "#") (equal? (first (fifth line)) "#"))
@@ -53,13 +63,19 @@
 
 (define (substitute-single ssa sub)
   (map (lambda (line) 
-         (map (lambda (phrase)
-                (if (equal? phrase (first sub)) (second sub) phrase))
-              line))
+         (if (equal? (drop line 2) (first sub))
+           (append (take line 2) (second sub))
+           (map (lambda (phrase)
+                  (if (equal? phrase (first sub)) (second sub) phrase))
+                line)))
        ssa))
 
 (define (reference? ssa)
   (equal? (first ssa) "%"))
+
+(define (remove-dead-pass ssa top)
+  (let ((next (remove-dead-ssa ssa top)))
+    (if (equal? next ssa) ssa (remove-dead-pass next top))))
 
 (define (remove-dead-ssa ssa top)
   (let* ((refs (map second (remove-duplicates (filter reference? (filter list? (append-map cdddr ssa))))))
@@ -71,4 +87,4 @@
 (let* ((out (generate-ssa '("if" ("=" 1 2) ("chain" ("turn" 1) ("move" 10)) ("chain" ("move" 5))) '() 0))
        (ssa (first out))
        (top (- (second out) 1)))
-  (pretty-print (fold-propagate-ssa ssa)))
+  (pretty-print (remove-dead-pass (fold-propagate-ssa ssa) top)))
