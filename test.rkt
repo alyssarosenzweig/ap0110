81943273d622de4860d22ba7aed720d1019fe493
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2016-10-22 11:07:34 -0700

    Show more detailed example

--- test.rkt
+++ test.rkt
@@ -5,8 +5,9 @@
 (require "typer.rkt")
 
 ;(let* ((out (generate-ssa '("if" ("=" 1 2) ("chain" ("turn" 1) ("move" 10)) ("chain" ("move" 5))) '() 0))
-(let* ((out (generate-ssa '("+" 1 1) '() 0))
+(let* ((out (generate-ssa '("+" "12" 1) '() 0))
         (ssa (first out))
         (top (- (second out) 1)))
+  (pretty-print ssa)
   (pretty-print (type-ssa ssa)))
   ;(pretty-print (remove-dead-pass (fold-propagate-pass ssa) top)))
